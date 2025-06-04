c972aa9feee1c5f85b6c72736fe4766324dfba6e
satishBohra-myriadai <satishBohra-myriadai@users.noreply.github.com>
2025-06-04 09:46:15 +0000

    chore: auto-build dist on push

---
+++ dist/interfaces/client-voice-gallery.interface.d.ts
@@ -0,0 +1,26 @@
+import { Types } from 'mongoose';
+import { CallingModel, VoiceType, VoiceSource, VoiceGender } from '../enums/user-enums';
+export interface IClientVoiceGallery {
+    voiceGalleryId: Types.ObjectId;
+    callingModel: CallingModel;
+    userId: Types.ObjectId;
+    tenantID: string;
+    displayAgentName: string;
+    type: VoiceType;
+    source: VoiceSource;
+    voiceId?: string;
+    voiceGender?: VoiceGender;
+    voiceName?: string;
+    fileUrl: string;
+    description: string;
+    isEnabled: boolean;
+    isCreditSufficient: boolean;
+    isDefault: boolean;
+    predefinedVoiceName: string;
+    createdAt: Date;
+    maxVoicesAllowed: number;
+    isVoicePermanentlyDisabled: boolean;
+    additionalMetadata?: Record<string, any>;
+    updatedAt: Date;
+}
+//# sourceMappingURL=client-voice-gallery.interface.d.ts.map
