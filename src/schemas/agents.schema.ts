0f25f2433a43e47666a8ef379ca10d89b83fdad6
myriadaibharat <bharat.gupta@myriadai.ai>
2025-06-17 13:15:54 +0530

    schema added

---
+++ src/schemas/agents.schema.ts
@@ -0,0 +1,31 @@
+// src/agent/entities/agent.entity.ts
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+export type AgentDocument = Agent & Document;
+
+@Schema({ timestamps: true })
+export class Agent {
+    @Prop({ required: true })
+    name!: string;
+
+    @Prop({ type: Object, required: true })
+    callTemplate!: any;
+
+    @Prop({ type: Object, required: true })
+    credentials!: any;
+
+    @Prop({ required: true, default: '' })
+    agentId!: string;
+
+    @Prop({ required: true, default: '' })
+    agentName!: string;
+
+    @Prop({ type:Types.ObjectId, required: true, default: '' })
+    voiceDbId!: Types.ObjectId;
+
+    @Prop({ type: Object, required: true, default: {} })
+    response!: any;
+}
+
+export const AgentSchema = SchemaFactory.createForClass(Agent);
