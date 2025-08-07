5f5d704fea968326087280c97eb50697f332724f
satishBohra-myriadai <satishBohra-myriadai@users.noreply.github.com>
2025-08-07 09:10:22 +0000

    chore: auto-build dist on push

--- dist/enums/user-enums.d.ts
+++ dist/enums/user-enums.d.ts
@@ -77,6 +77,10 @@ export declare enum CampaignType {
     INBOUND = "Inbound",
     ONDEMAND = "OnDemand"
 }
+export declare enum CampaignMode {
+    AGENT_IN = "AgentIn",
+    NORMAL = "Normal"
+}
 export declare enum ContactSourceType {
     FILE = "File",
     CONTACT_LIST_ID = "ContactListId",
