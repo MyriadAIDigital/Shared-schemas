5b5527aeb32d318a49a96af8ad7bd898c816ab13
satish <sateesh@gobillion.co>
2025-10-28 19:50:01 +0530

    fix: update humanAgentNumber property to use an empty string as default

--- src/schemas/campaign.schema.ts
+++ src/schemas/campaign.schema.ts
@@ -61,8 +61,8 @@ export class Campaign {
     @Prop({ required: true })
     outboundPhoneNo!: string;
 
-    @Prop({ required: false, default: null })
-    humanAgentNumber!: string | null;
+    @Prop({ type: String, required: false, default: '' })
+    humanAgentNumber!: string;
 
 
     @Prop({ default: true })
